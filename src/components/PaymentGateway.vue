24f14553b184a91ad0d72ff6ba7d40fc0efce0f7
Bugra AYAN* <112655530+bugratr@users.noreply.github.com>
2023-10-15 18:38:07 +0300

    Create PaymentGateway.vue

---
+++ src/components/PaymentGateway.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="payment-gateway">
+        <h2>Payment Gateway</h2>
+
+        <section v-if="isTenant">
+            <h3>Make Payment</h3>
+            <div>
+                <label for="amount">Amount (in Ether):</label>
+                <input type="number" v-model="paymentAmount" id="amount" required>
+            </div>
+            <button @click="makePayment">Pay Rent</button>
+        </section>
+
+        <section>
+            <h3>Payment History</h3>
+            <ul v-if="payments.length">
+                <li v-for="payment in payments" :key="payment.id">
+                    {{ payment.date }} - {{ payment.amount }} Ether - {{ payment.status }}
+                </li>
+            </ul>
+            <p v-else>No payments recorded.</p>
+        </section>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isTenant: false,  // To determine if the user is a tenant for the agreement
+            paymentAmount: 0,
+            payments: []  // Array to store the list of payments made/received
+        }
+    },
+    created() {
+        this.determineUserRole();
+        this.fetchPayments();
+    },
+    methods: {
+        determineUserRole() {
+            // Logic to check if the user is a tenant or a landlord for the agreement
+            // Based on the result, update the 'isTenant' data property
+        },
+        async fetchPayments() {
+            // Logic to interact with the Ethereum blockchain and retrieve payment details/history
+            // This would involve using a library like Web3.js or Ethers.js
+            // Populate the 'payments' array with the fetched data
+        },
+        async makePayment() {
+            // Logic for the tenant to make a rent payment
+            // This would interact with the Ethereum blockchain, transferring the payment amount
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* Basic styling for the payment gateway */
+.payment-gateway {
+    max-width: 500px;
+    margin: 0 auto;
+    padding: 20px;
+}
+
+button {
+    margin-top: 10px;
+    padding: 5px 15px;
+}
+</style>
